0158022d629e99d74ba97ec5bc3b232809a778c3
B10sGameDev <92919070+BlasDiez@users.noreply.github.com>
2024-09-29 17:51:31 +0200

    First Commit

---
+++ Test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html  lang="en">
+<head>
+    <title>Juan Carlos precidente</title>
+</head>
+<body>
+    <?php
+        echo "Esto es un bloque";
+        echo "de código correcto";
+    ?>
+
+    <?php
+    echo "Esto es un bloque";
+    echo "de código correcto"
+    ?>
+
+    <?php
+    echo "Esto es un bloque de código correcto"
+    ?>
+</body>
+
+</html>
